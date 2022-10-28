a676d874346dac7579414f14749c7c2545c8826c
matheusbalduino <matheus.balduin@gmail.com>
2022-10-28 00:24:59 -0300

    effects

--- src/app/Index/index/index.component.ts
+++ src/app/Index/index/index.component.ts
@@ -1,4 +1,6 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit, HostListener } from '@angular/core';
+import { Scroll } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -10,8 +12,11 @@ export class IndexComponent implements OnInit {
   menu:string = "menu";
   clickMenu: boolean = false;
   colNumber: string = "3"
+  effectOn: boolean = false;
+  effectOn2: boolean = false;
   public getScreenWidth: any;
   public getScreenHeight: any;
+  public lastPositionY:any;
 
   constructor() { }
 
@@ -32,6 +37,32 @@ export class IndexComponent implements OnInit {
     this.getScreenHeight = window.innerHeight;
     if(this.getScreenWidth > 748 ) this.clickMenu = false;
   }
+  
+  count:number = 0
+
+  @HostListener('window:scroll', ['$event'])
+  onScrollDown(){
+    const elementBox1 = document.getElementById("boxResume")
+    const heightBox1 = elementBox1?.scrollHeight || 0
+
+    const elementBox2 = document.getElementById("boxResume2")
+    const heightBox2 = elementBox2?.scrollHeight || 0
+
+    this.lastPositionY = window.scrollY;
+
+    if(this.lastPositionY > heightBox1 + 200) this.effectOn = true
+    else this.effectOn = false
+    if(this.lastPositionY > heightBox2 - 100) this.effectOn2 = true
+    else this.effectOn2 = false
+  }
+
+  onClickHome(){
+    const elementBox1 = document.getElementById("boxResume")
+    const heightBox1 = elementBox1?.scrollHeight || 0
+    window.scrollTo(0, heightBox1 + 800)
+    console.log('click')
+  }
+
 
 
 }
